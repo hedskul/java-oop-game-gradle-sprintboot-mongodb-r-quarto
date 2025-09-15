c0b49b802450e1f3e6f40cc64afdd62cca0b7608
hedskul <166876619+hedskul@users.noreply.github.com>
2025-09-14 20:33:03 -0400

    r studio

---
+++ samples/renv-scripts.R
@@ -0,0 +1,53 @@
+## nuke renv
+if (requireNamespace("renv", quietly = TRUE)) renv::deactivate()
+unlink("renv", recursive = TRUE, force = TRUE)
+unlink("renv.lock", force = TRUE)
+# rprofile
+if (file.exists(".Rprofile")) {
+  txt <- readLines(".Rprofile", warn = FALSE)
+  writeLines(txt[!grepl("renv/activate\\.R", txt)], ".Rprofile")
+}
+# disconnect
+q(save = "no")
+
+# init
+install.packages("renv")               
+renv::init(bare = TRUE)  
+
+renv::snapshot()  
+
+# include
+renv::install(c(
+  "tidyverse",     
+  "readxl",        
+  "reticulate",    
+  "knitr",         
+  "rmarkdown"      
+))
+
+#lock
+renv::snapshot()  
+
+# if (!requireNamespace("rmarkdown", quietly = TRUE)) install.packages("rmarkdown")
+# if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv")
+# if (!renv::project()) renv::activate()
+# renv::snapshot()
+
+
+
+
+
+
+
+
+# # if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv")
+# #
+# # if (!renv::project()) renv::activate()
+# 
+# required <- c("reticulate", "knitr", "rmarkdown")
+# for (pkg in required) {
+#   if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
+# }
+# 
+# # Set global chunk options
+# knitr::opts_chunk$set(echo = TRUE)
